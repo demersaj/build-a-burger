7926b208554dbaa1f2d4f15588a24a84ed5fb1d7
Andrew Demers <andrew.demers91@gmail.com>
2019-01-18 17:56:35 -0600

    Fixed Redux and refactored.

---
+++ src/assets/apiKey.js
@@ -0,0 +1,3 @@
+const apiKey = 'AIzaSyCTTKnZGDZnPgzaiCKlP3Is-a5BTSSBYM4';
+
+export default apiKey;
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 import Input from '../../../components/UI/Input/Input';
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
+import * as actions from '../../../store/actions/index';
 
 class ContactData extends Component {
 	state = {
@@ -89,35 +92,26 @@ class ContactData extends Component {
 				valid: true
 			}
 		},
-		formIsValid: false,
-		loading: false
+		formIsValid: false
 	};
 
 	orderHandler = ( event ) => {
 		event.preventDefault();
-		this.setState({ loading: true });
 		const formData = {};
 		for (let formElementIdentifier in this.state.orderForm) {
 			formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
 		}
 
 		const order = {
-			ingredients: this.props.ingredients,
+			ingredients: this.props.ings,
 			price: this.props.price,
 			orderData: formData
 		};
 
-		axios.post('/orders.json', order)
-			.then(response => {
-				this.setState({ loading: false });
-				this.props.history.push('/');   // routes back to homepage after orerHandler executes
-			})
-			.catch(error => {
-				this.setState({ loading: false });
-			});
+		this.props.onOrderBurger(order);
 	};
 
-	static checkValidity( value, rules ) {
+	checkValidity( value, rules ) {
 		let isValid = true;
 
 		if (rules.required) {
@@ -131,7 +125,14 @@ class ContactData extends Component {
 		if (rules.maxLength) {
 			isValid = value.length <= rules.maxLength && isValid;
 		}
-
+		if (rules.isEmail) {
+			const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+			isValid = pattern.test(value) && isValid
+		}
+		if (rules.isNumeric) {
+			const pattern = /^\d+$/;
+			isValid = pattern.test(value) && isValid
+		}
 		return isValid;
 	}
 
@@ -149,7 +150,7 @@ class ContactData extends Component {
 
 		let formIsValid = true;
 		for (let inputIdentifier in updatedOrderForm) {
-			formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid
+			formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
 		}
 
 		this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
@@ -182,7 +183,7 @@ class ContactData extends Component {
 				<Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
 			</form>
 		);
-		if (this.state.loading) {
+		if (this.props.loading) {
 			form = <Spinner />;
 		}
 		return (
@@ -192,7 +193,20 @@ class ContactData extends Component {
 			</div>
 		);
 	}
-
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+	return {
+		ings: state.burgerBuilder.ingredients,
+		price: state.burgerBuilder.totalPrice,
+		loading: state.order.loading
+	};
+};
+
+const mapDispatchToProps = dispatch => {
+	return {
+		onOrderBurger: ( orderData ) => dispatch(actions.purchaseBurger(orderData))
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
