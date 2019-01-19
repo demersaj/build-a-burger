406f7e906e9cd6cf70a6eb1039ceed31e5cb231d
Andrew Demers <andrew.demers91@gmail.com>
2019-01-19 17:06:54 -0600

    Finished testing and hosting

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;    // needed to use Redux dev tools
+const composeEnhancers = process.env.NODE_ENV == 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__: null || compose;
+// needed to use Redux dev tools
 
 const rootReducer = combineReducers({
 	burgerBuilder: burgerBuilderReducer,
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -56,7 +56,6 @@ export const auth = ( email, password, isSignup ) => {
 
 		axios.post(url, authData)
 			.then(response => {
-				console.log(response);
 				const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
 				localStorage.setItem('token', response.data.idToken);
 				localStorage.setItem('expirationDate', expirationDate);
@@ -65,10 +64,8 @@ export const auth = ( email, password, isSignup ) => {
 				dispatch(checkAuthTimeout(response.data.expiresIn));
 			})
 			.catch(err => {
-				console.log(err);
 				dispatch(authFail(err.response.data.error));
 			});
-		console.log('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=' + apiKey, +authData);
 	};
 };
 
